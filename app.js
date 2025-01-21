1aa8ffc55bc9d2167edea8f5ac997312f79df80b
ShabaylovSB <ShabaylovSB@vzljot.ru>
2025-01-21 10:24:43 +0300

    fix(app.js): 02 stash with params

--- app.js
+++ app.js
@@ -8,15 +8,7 @@ console.log(
   })
 )
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 console.log('1. 04 hello ++ !!! ++++ main title', )
-=======
-console.log('1. 04 hello ++ !!! main title', )
->>>>>>> Stashed changes
-=======
-console.log('1. 04 hello ++ !!! main title', )
->>>>>>> Stashed changes
 
 console.log('2. sub title', )
 
